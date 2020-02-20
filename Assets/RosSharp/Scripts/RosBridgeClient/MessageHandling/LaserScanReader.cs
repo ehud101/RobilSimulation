ce4a9ac16341210bb920a161294aad32ae6d42ba
Robil <robil@deepspace>
2020-02-20 13:46:36 +0200

    code refactoring  and description was added for the LaserScanReader and publisher The option to set the min and max angle removed and now we always scan 360

--- Assets/RosSharp/Scripts/RosBridgeClient/MessageHandling/LaserScanReader.cs
+++ Assets/RosSharp/Scripts/RosBridgeClient/MessageHandling/LaserScanReader.cs
@@ -1,17 +1,15 @@
 ﻿
-﻿/*
-© Siemens AG, 2018-2019
-Author: Berkay Alp Cakal (berkay_alp.cakal.ct@siemens.com)
+/*
+RPLidar
+Auther: Mor Eliyahu
+Date: 19.2.20
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-<http://www.apache.org/licenses/LICENSE-2.0>.
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+This code simulates the RPLidar A2 and transfer the data received to ROS LaserScan message.
+Every Fixed update a number of measurments are calculated according to the the time of a single measurment of the RPLidar A2 and the wanted resolution, 
+set by the user. After we calculate a full cycle of measurments (360), we transfer the data in LaserScan message to the LaserScanPublisher. 
+In order to get the scan we use RaycastCommand and Raycast of the unity that determines if a single ray from one point to another hit a collider and do it 
+asynchronously and in parallel to each other. 
+##A We use 360 deg cycle of the RP Lidar because that worked good with the hector slam
 */
 
 using UnityEngine;
@@ -30,69 +28,63 @@ namespace RosSharp.RosBridgeClient
     
     public class LaserScanReader : MonoBehaviour {
         
-        //rp lidar node in the simulation containes a single measurment of distance and intensity. In order to work with the real sensor we will need to change the struct to match the real message from the rplidar.
-        //angle and distance of 32 measurments
+        //This struct containes the angle and distance of the measurments. we save the data of 1 cycle because that is what the hector slam expacts the get in LaserScan message.
         public class rplidar__data{
             //distance im m
-            public float[] dist = new float[400];
-            //float quality = new float[32];
+            public float[] dist;
             //angle in degrees
-            public float[] angle = new float[400];
+            public float[] angle;
+            //float quality;
         }
-
+        //A publisher to send the RpLidar data for hector slam
         public LaserScanPublisher LaserScanPublisher;
+        //Used for the laser scan
         public Transform  SensorRotator;
-        //angle resolution set by the user
+        //Angle resolution set by the user[deg]
         public float angle_res;
-        // start angle of the scan set by the user[deg]
-        public float MinAngle;
-        //end angle of the scan set by the user[deg]
-        public float MaxAngle;
         
+        //##A  Start angle of the scan[deg] -
+        private float MinAngle = 0;
+        //##A End angle of the scan[deg]
+        private float MaxAngle = 360;
+        //The message with the RPLidar data to publish for the hector slam
         private Messages.Sensor.LaserScan message;
         //When this array is filled with the distances data of the scans, we send the data message with the publisher
         private rplidar__data lidar_data;
         private int measurmensts_counter;
         //RP Lidar A2 time for a single measurment
         private float measurment_time = 0.00025f;
-        //calculated according to the angle resolution set by the user
-        private float rotationFrequency;
-        //calculate the number of laser samples that should be taken in fixedupdate time according to the measurment time
+        //Calculate the number of laser samples that should be taken in fixedupdate time according to the measurment time
         private int SamplesPerPhysStep;
         private float horCurrentAngle;
+        //This array get the results of the raycastcommands that were performed
         private NativeArray<RaycastHit> results;
+        //This array save the raycast commands to perform
         private NativeArray<RaycastCommand> commands; 
         private float[] azimuth;
-        //private LaserScanVisualizer[] laserScanVisualizers;
-
         
 
         //**check this***/
-        //time between measurements [seconds] - if your scanner is moving, this will be used in interpolating position of 3d points
+        //Time between measurements [seconds] - if your scanner is moving, this will be used in interpolating position of 3d points
         private float time_increment;
         //*******check this and also missing in our code msg********/
-        //time between scans [seconds]
+        //Time between scans [seconds]
         private float scan_time;
-        //# intensity data [device-specific units].  If your device does not provide intensities, please leave the array empty
-        private float[] intensities;
 
         public void Start()
         {
             lidar_data = new rplidar__data();
+            //The total measurments in 1 cycle
+            int totalMeasurments =  Convert.ToInt32(Mathf.Floor((MaxAngle - MinAngle) / angle_res));
+            SamplesPerPhysStep = Mathf.RoundToInt(Time.fixedDeltaTime / measurment_time); 
+            lidar_data.dist = new float[totalMeasurments];
+            lidar_data.angle = new float[totalMeasurments];
             message = new Messages.Sensor.LaserScan();
             message.ranges = new float[lidar_data.dist.Length];
-            //we don't get intensities but need to send an empty array
-            intensities = new float[lidar_data.dist.Length];
-            MinAngle = MinAngle < 0 ? 360 + MinAngle : MinAngle;
-            MaxAngle = MaxAngle < 0 ? 360 + MaxAngle : MaxAngle;
-            rotationFrequency = 1f / ((MaxAngle - MinAngle) / angle_res * measurment_time);
-            SamplesPerPhysStep = Mathf.RoundToInt(Time.fixedDeltaTime / measurment_time); 
-            //results = new NativeArray<RaycastHit>(SamplesPerPhysStep, Allocator.TempJob);
-            //commands = new NativeArray<RaycastCommand>(SamplesPerPhysStep, Allocator.TempJob);
             horCurrentAngle = MinAngle;
+            //Move the scanner to the start angle of the scan
             SensorRotator.localEulerAngles = new Vector3(0, horCurrentAngle, 0);
             azimuth = new float[SamplesPerPhysStep];
-
             //*****check this - i think it's the total time for tis scan. maybe need to measure the time since start filling the array until finish */
             scan_time = lidar_data.dist.Length * measurment_time;
             //*****check this - i think it's the time for each laser scan */
@@ -110,37 +102,38 @@ namespace RosSharp.RosBridgeClient
             var results = new NativeArray<RaycastHit>(SamplesPerPhysStep, Allocator.TempJob);
             var commands = new NativeArray<RaycastCommand>(SamplesPerPhysStep, Allocator.TempJob);
             int i;
-
+/*
             if (horCurrentAngle >= MaxAngle && horCurrentAngle < MinAngle)
             { //completed horizontal scan
                 horCurrentAngle = MinAngle;
                 SensorRotator.localEulerAngles = new Vector3(0, MinAngle, 0);
-            }
+            }*/
             if (horCurrentAngle >= 360)
             { //completed horizontal scan
                 horCurrentAngle = 0;
                 SensorRotator.localEulerAngles = new Vector3(0, 0, 0);
             }
 
-            Vector3 scanerPos = SensorRotator.position;//  Vector3 ScanerLinearStep = ScannerVel * Time.fixedTime;
+            Vector3 scanerPos = SensorRotator.position;
             int add_val = 0;
             float start_ang = horCurrentAngle;
             for (i = 0; i < SamplesPerPhysStep; i++)
             {
                 //Debug.Log("horCurrentAngle " + horCurrentAngle + " max " + _maxAngle + " min " + _minAngle);
+                /*
                 if (horCurrentAngle >= MaxAngle && horCurrentAngle < MinAngle)
                 {
                     start_ang = MinAngle;
                     SensorRotator.localEulerAngles = new Vector3(0, MinAngle, 0);
                     add_val = 0;
-                }
+                }*/
                 if (horCurrentAngle >= 360)
                 {
                     start_ang = 0;
                     SensorRotator.localEulerAngles = new Vector3(0, 0, 0);
                     add_val = 0;
                 }
-
+                //Insert raycast commend to the raycast commends array
                 commands[i] = new RaycastCommand(scanerPos, SensorRotator.TransformDirection(Vector3.forward), 6);
 
                 //if (DrawLidarDebug)
@@ -148,8 +141,9 @@ namespace RosSharp.RosBridgeClient
                 
                 add_val++;
                 azimuth[i] = horCurrentAngle;
-                Debug.Log (horCurrentAngle);
+                //Debug.Log (horCurrentAngle);
                 horCurrentAngle = start_ang + add_val * angle_res;
+                //move the scenner by angle_res
                 SensorRotator.localEulerAngles = new Vector3(0, horCurrentAngle, 0);
             }
 
@@ -161,13 +155,13 @@ namespace RosSharp.RosBridgeClient
 
             // commands.Dispose();
 
+            //Take the data from the results array
             for (int loc = 0; loc < results.Length; loc++)
             {
                 var raycastHit = results[loc];
-             
-                if (raycastHit.collider != null){
 
-                
+                if (raycastHit.collider != null){
+                    //Insert the distance of the hit
                     lidar_data.dist[measurmensts_counter] = raycastHit.distance;
                        //Debug.Log("raycastHit" + raycastHit.distance);
                 }
@@ -176,7 +170,9 @@ namespace RosSharp.RosBridgeClient
                      lidar_data.dist[measurmensts_counter] = 0;
                      //Debug.Log("raycastHit" + raycastHit.distance);
                 }
+                //Insert the angle of the relevant distance
                 lidar_data.angle[measurmensts_counter] = azimuth[loc];
+                //Check if you fill the array in order to send the message
                 if(measurmensts_counter == lidar_data.dist.Length -1){
                     measurmensts_counter = 0;
                     //for(int j = 0; j < 32; j++){
